36f77d31bab1eaa5a171da5da4c0337a48d47bff
Arodlives <46770499+Arodlives@users.noreply.github.com>
2023-02-09 17:43:51 -0500

    completed Header with scroll ,next is Banner

---
+++ components/Header.tsx
@@ -0,0 +1,102 @@
+import Image from "next/image"
+import Link from "next/link";
+import netflix from '../public/netflix.png'
+import { IconUser,IconSearch,IconBell,IconQuestionCircle,IconPencil } from '@tabler/icons';
+import { useEffect, useState } from "react";
+
+
+function Header() {
+    const[isScrolled,setIsScrolled]=useState(false)
+
+    useEffect(()=>{ //? runs on one single mount
+        const handleScroll =()=>{
+            if (window.scrollY > 0){
+                setIsScrolled(true)
+            } else{
+                setIsScrolled(false)
+            }
+        }
+
+        window.addEventListener("scroll",handleScroll)
+        
+        return ()=>{
+            window.removeEventListener("scroll",handleScroll)
+        }
+
+    },[])
+
+
+
+  return (
+    <header className={`${isScrolled && 'bg-[#000000]'}`}>
+        <div className="flex items-center space-x-2 md:space-x-10">
+            <Image className="cursor-pointer object-contain" src={netflix} width={100} height={100} alt="Netflix logo" />
+
+            <ul className="hidden space-x-4 md:flex">
+                <li className="headerLink ">Home</li>
+                <li className="headerLink ">Tv Shows</li>
+                <li className="headerLink">Movies</li>
+                <li className="headerLink">New & Popular</li>
+                <li className="headerLink">My List</li>
+                <li className="headerLink">Browse by languages</li>
+            </ul>
+
+        </div>
+
+        {/*Right section of Navbar */}
+        <div className="flex  items-center text-sm font-light underline space-x-4">
+        <IconSearch className="hidden sm:inline cursor-pointer " >
+            
+        </IconSearch>
+        
+        <IconBell className="hidden sm:inline cursor-pointer">
+        </IconBell>
+
+        {/* <avatar>
+            <ul></ul>
+            <ul>
+                <div className="flex">
+                    <p>Manage Profile</p>
+                    <Link href="/profiles/manage">
+                        <IconPencil className="hidden sm:inline h-6 w-6 "> 
+                        </IconPencil>
+                    </Link>
+                </div>
+            </ul>
+            <ul>
+                <div className="flex">
+                    <p>Transfer Profile</p>
+                    <Link href="/account">
+                        <IconUser className="hidden sm:inline h-6 w-6 "> 
+                        </IconUser>
+                    </Link>
+                </div>
+            </ul>
+
+            <ul>
+                <div className="flex">
+                    <p>Account</p>
+                    <Link href="/account">
+                        <IconUser className="hidden sm:inline h-6 w-6 "> 
+                        </IconUser>
+                    </Link>
+                </div>
+            </ul>
+
+            <ul>
+                <div className="flex">
+                    <p>Help Center</p>
+                    <Link href="/helpcenter">
+                        <IconQuestionCircle className="hidden sm:inline h-6 w-6 "> 
+                        </IconQuestionCircle>
+                    </Link>
+                </div>
+            </ul>
+        </avatar> */}
+
+        </div>
+    </header>
+  )
+}
+
+export default Header
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,19 +2,22 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import Header from 'components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div>
+    //? 😮‍💨Add color styles/themes option using state 
+    <div className="relative h-screen bg-gradient-to-b from-transparent/10 to-[#376df6] lg:h-[140vh]">
       <Head>
         <title>Netflix</title>
         <meta name="description" content="Aaron's Netflix 2.0" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      Lets build netflix
+
+      <Header/>
       {/* Header */}
       <main>
         {/*Banner*/}
